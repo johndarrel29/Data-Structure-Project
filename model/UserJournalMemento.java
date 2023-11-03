69e808e11d17d20ba5c28f4b4fcdea5ff9264e0c
theaneryy00 <111473139+theaneryy00@users.noreply.github.com>
2023-11-03 22:01:07 +0800

    Update UserJournalMemento.java

--- model/UserJournalMemento.java
+++ model/UserJournalMemento.java
@@ -2,12 +2,25 @@ package model;
 
 public class UserJournalMemento{
     private String journal;
+    private String title;
 
-    public UserJournalMemento(String journal) {
+    public UserJournalMemento(String title, String journal) {
         this.journal = journal;
+        this.title = title;
     }
+
+    public UserJournalMemento(String journalText, String insertJournal, String title, String username) {
+    }
+
+   
 
     public String getJournal() {
         return journal;
     }
+
+    public String getTitle() {
+        return null;
+    }
+
+    
 }
