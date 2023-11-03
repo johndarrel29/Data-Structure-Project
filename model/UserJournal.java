9502f387af091486e8e1bcaf4ad36c4bd79e05a9
theaneryy00 <111473139+theaneryy00@users.noreply.github.com>
2023-11-03 22:01:25 +0800

    Update UserJournal.java

--- model/UserJournal.java
+++ model/UserJournal.java
@@ -1,21 +1,38 @@
 package model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class UserJournal {
-    private String journalText;
+    private final StringProperty journalText;
+    private final StringProperty title;
 
-    public UserJournal() {
-        // Public no-argument constructor
-    }
-
-    public UserJournal(String journalText) {
-        this.journalText = journalText;
+    public UserJournal(String journalText, String title, String string) {
+        this.journalText = new SimpleStringProperty(journalText);
+        this.title = new SimpleStringProperty(title);
     }
 
     public String getJournalText() {
+        return journalText.get();
+    }
+
+    public String getTitle() {
+        return title.get();
+    }
+
+    public StringProperty journalTextProperty() {
         return journalText;
     }
 
+    public StringProperty titleProperty() {
+        return title;
+    }
+
     public void setJournalText(String journalText) {
-        this.journalText = journalText;
+        this.journalText.set(journalText);
+    }
+
+    public void setTitle(String title) {
+        this.title.set(title);
     }
 }
